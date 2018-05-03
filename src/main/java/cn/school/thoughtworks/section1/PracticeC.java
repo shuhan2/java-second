339228705ce563b2c60e77de796b7e1d7f4c245c
shuhan2 <2472520650@qq.com>
2018-05-03 21:52:02 +0800

    1-C

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,20 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        //选出1集合中跟2对象中value属性中的元素相同的元素
+//        List<String> stream =new ArrayList<>();
+//        for(List<String> list : collection2){
+//            stream.addAll(list);
+//        }
+        return collection1.stream().filter(item -> {for(List<String> value : collection2.values()){return value.contains(item);}return false;}).collect(toList());
+
     }
 }
