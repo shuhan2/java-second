a1694245988ff6a103f03b7bbe1e8c2c886e4968
shuhan2 <2472520650@qq.com>
2018-05-03 16:24:04 +0800

    init

---
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -0,0 +1,14 @@
+package cn.school.thoughtworks.section1;
+
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
+
+public class PracticeA {
+    List<String> collectSameElements(List<String> collection1, List<String> collection2) {
+        //实现练习要求，并改写该行代码。
+       
+    }
+
+
+}
